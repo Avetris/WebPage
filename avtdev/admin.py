b216543379bddde3d059a0890df39962757153c5
SarkV <aitorvt95@gmail.com>
2020-09-30 00:32:12 +0200

    Added messages to admin

--- avtdev/admin.py
+++ avtdev/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Development, Info
+from .models import Development, Info, Message
 
 admin.site.register(Development)
 admin.site.register(Info)
+admin.site.register(Message)
